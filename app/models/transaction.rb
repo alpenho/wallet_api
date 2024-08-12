2e7bd27e5214d7184f78b8fe6acf79c27da60bcb
Alpen Halim <alpenho@gmail.com>
2024-08-13 00:05:29 +0700

    feat: Implement enum and validation for transaction and create a transaction service

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,10 @@
 class Transaction < ApplicationRecord
-  belongs_to :source_wallet_id, class_name: 'Wallet'
-  belongs_to :target_wallet_id, class_name: 'Wallet'
+  belongs_to :source_wallet, class_name: 'Wallet'
+  belongs_to :target_wallet, class_name: 'Wallet'
+
+  enum transaction_type: [ :deposit, :withdraw, :transfer ]
+
+  validates :transaction_type, presence: true
+  validates_presence_of :source_wallet, unless: :deposit?
+  validates_presence_of :target_wallet, unless: :withdraw?
 end
---
+++ app/services/transaction.rb
@@ -0,0 +1,14 @@
+class Services::Transaction
+  def init(wallet)
+    @wallet = wallet
+  end
+
+  def deposit
+  end
+
+  def withdraw
+  end
+
+  def transfer(target_wallet)
+  end
+end
