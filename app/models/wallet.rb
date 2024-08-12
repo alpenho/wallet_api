ae74952c8f10230f5ea93741de39422c55ced984
Alpen Halim <alpenho@gmail.com>
2024-08-12 22:53:21 +0700

    feat: Implement has many relationship in wallet table with transaction table

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,7 @@
 class Wallet < ApplicationRecord
   belongs_to :owner, polymorphic: true
+  has_many :outgoing_transactions, class_name: 'Transaction', 
+                            foreign_key: 'source_wallet_id'
+  has_many :incoming_transactions, class_name: 'Transaction',
+                            foreign_key: 'target_wallet_id'
 end
