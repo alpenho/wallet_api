3d4e2e8ad1883d8343a829039ab350eaa8e2c73c
Alpen Halim <alpenho@gmail.com>
2024-08-13 00:19:31 +0700

    fix: Fixing validation for transaction when transaction type is deposit and withdraw

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction < ApplicationRecord
-  belongs_to :source_wallet, class_name: 'Wallet'
-  belongs_to :target_wallet, class_name: 'Wallet'
+  belongs_to :source_wallet, class_name: 'Wallet', optional: true
+  belongs_to :target_wallet, class_name: 'Wallet', optional: true
 
   enum transaction_type: [ :deposit, :withdraw, :transfer ]
 
   validates :transaction_type, presence: true
-  validates_presence_of :source_wallet, unless: :deposit?
-  validates_presence_of :target_wallet, unless: :withdraw?
+  validates :source_wallet, presence: true, unless: :deposit?
+  validates :target_wallet, presence: true, unless: :withdraw?
 end
